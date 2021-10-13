7d1dd96e85c8935ce4e6ff9f167c4ae6ac76fb9f
JoelWiegand <16wiegandjr@gmail.com>
2021-10-13 12:16:49 -0500

    small changes

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -3,7 +3,6 @@ import { Post } from 'src/app/models/post';
 import { PostService } from './../../services/post.service';
 import { Component, OnInit } from '@angular/core';
 import { ClientMessage } from 'src/app/models/client-message';
-import { waitForAsync } from '@angular/core/testing';
 
 @Component({
   selector: 'app-post',
@@ -20,22 +19,22 @@ export class PostComponent implements OnInit {
 
   constructor(private postServ: PostService) {
    
-
   }
-
+  
   ngOnInit(): void {
     this.findAllPosts()
     this.setUsers();
   }
+  
   public findAllPosts() {
     this.postServ.findAllPosts() // every object that is captured from the observabel is set as our users array
       .subscribe(data => { this.posts = data }) // this defines what we do with the data returned from the observable
-   
-  }
-  public setUsers() {
     this.postServ.findAllPostUsers() // every object that is captured from the observabel is set as our users array
       .subscribe(data => { this.users = data }) // this defines what we do with the data returned from the observable
-    for (let index = 0; index < this.posts.length; index++) {
+    
+  }
+  public setUsers() {
+   for (let index = 0; index < this.posts.length; index++) {
       this.posts[index].userId = this.users[index];
       console.log(this.posts[index]);
     }
